29d5fa84466577099e212e0e986b2eb06d833aa6
ABT Aurelien <aurelien@abrassart.net>
2024-06-18 16:42:13 +0200

    Ajoutez des fichiers projet.

---
+++ Week3-Q-Classes-Videogame/Character.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Week3_Q_Classes_Videogame
+{
+    public class Character
+    {
+        // Propriétés
+        private string _name;
+        private int _lifepoint;
+        private int _strength;
+        private int _defense;
+
+
+        public Character(string name, int lifepoint, int strength, int defense)
+        {
+            _name = name;
+            _lifepoint = lifepoint;
+            _strength = strength;
+            _defense = defense;
+        }
+
+        public bool IsAlive()
+        {
+            return (_lifepoint <= 0 ? false : true);
+        }
+        public int GetStrength()
+        {
+            return _strength;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public void AttackedBy(Character character)
+        {
+            Console.WriteLine($"{_name} est attaqué par {character.GetName()}");
+            _lifepoint = _lifepoint - (character.GetStrength() - _defense);
+        }
+        public void PrintCharacter()
+        {
+            Console.WriteLine($"Nom du personnage : {_name}");
+            Console.WriteLine($"Point de vie : {_lifepoint}");
+            Console.WriteLine($"Force : {_strength}");
+            Console.WriteLine($"Défense : {_defense}");
+            Console.WriteLine("");
+        }
+
+    }
+
+}
---
+++ Week3-Q-Classes-Videogame/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Week3_Q_Classes_Videogame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // itinialisation des personnages
+            Character bob = new Character("Bob", 20, 10, 5);
+            Character ana = new Character("Ana", 18, 12, 7);
+            // On affiche chaque personnages
+            bob.PrintCharacter();
+            ana.PrintCharacter();
+
+            // Boucle de combat, bob est attaqué par ana puis ana est attaqué par bob)
+            while (bob.IsAlive() && ana.IsAlive())
+            {
+                bob.AttackedBy(ana);
+                ana.AttackedBy(bob);
+            }
+            // Désigne le vainqueur
+            if (bob.IsAlive())
+            {
+                Console.WriteLine("Bob a gagné !");
+            } else
+            {
+                Console.WriteLine("Ana a gagné !");
+            }
+        }
+    }
+}

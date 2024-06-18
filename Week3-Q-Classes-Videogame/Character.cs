01f11ba59b8ea1c7b480547c03196e2c31006bba
ABT Aurelien <aurelien@abrassart.net>
2024-06-18 16:45:16 +0200

    rename few variables

--- Week3-Q-Classes-Videogame/Character.cs
+++ Week3-Q-Classes-Videogame/Character.cs
@@ -5,22 +5,22 @@ namespace Week3_Q_Classes_Videogame
     {
         // Propriétés
         private string _name;
-        private int _lifepoint;
+        private int _healthpoint;
         private int _strength;
         private int _defense;
 
 
-        public Character(string name, int lifepoint, int strength, int defense)
+        public Character(string name, int healthpoint, int strength, int defense)
         {
             _name = name;
-            _lifepoint = lifepoint;
+            _healthpoint = healthpoint;
             _strength = strength;
             _defense = defense;
         }
 
         public bool IsAlive()
         {
-            return (_lifepoint <= 0 ? false : true);
+            return (_healthpoint <= 0 ? false : true);
         }
         public int GetStrength()
         {
@@ -33,12 +33,12 @@ namespace Week3_Q_Classes_Videogame
         public void AttackedBy(Character character)
         {
             Console.WriteLine($"{_name} est attaqué par {character.GetName()}");
-            _lifepoint = _lifepoint - (character.GetStrength() - _defense);
+            _healthpoint = _healthpoint - (character.GetStrength() - _defense);
         }
         public void PrintCharacter()
         {
             Console.WriteLine($"Nom du personnage : {_name}");
-            Console.WriteLine($"Point de vie : {_lifepoint}");
+            Console.WriteLine($"Point de vie : {_healthpoint}");
             Console.WriteLine($"Force : {_strength}");
             Console.WriteLine($"Défense : {_defense}");
             Console.WriteLine("");
